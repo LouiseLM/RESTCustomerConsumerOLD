49d3ac440b9c48bcc1cd718fa6825d0db820de8e
Louise Lytken Petersen Møller <loui142j@edu.easj.dk>
2019-03-21 11:02:21 +0100

    Add project files.

---
+++ RestCustomerConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestCustomerConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
